f3324d908ac2df8156022ddc989282937954086c
Craig Sharkie <craig.sharkie@gmail.com>
2013-08-19 12:18:45 +1000

    SCRIPTS Commented the scripts
    Commented the scripts out, but left them in. Saves a 404 from the
    boilerplate and also shows vanilla JavaScript reliance.
    document.body.classList.add FTW

---
+++ scripts/scripts.js
@@ -0,0 +1,17 @@
+/*
+var nudged = false;
+var nudge = function() {
+  if (nudged) {
+    document.body.classList.remove("nudged");
+    nudged = false;
+  }
+  else {
+    document.body.classList.add("nudged");
+    nudged = true;
+  }
+}
+
+document.body.addEventListener("keydown", nudge, false);
+document.body.addEventListener("keyup", nudge, false);
+*/
+
